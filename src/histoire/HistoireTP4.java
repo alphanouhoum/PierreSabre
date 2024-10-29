aea0583c8165b3394237f92f1f3c6c5be6c060cf
unknown <alpha-nouhoum.toure@univ-tlse3.fr>
2024-10-29 11:17:37 +0100

    création de la classe Humain

---
+++ src/histoire/HistoireTP4.java
@@ -0,0 +1,12 @@
+package histoire;
+
+public class HistoireTP4 {
+	
+	
+	
+	
+	
+	public main(void) {
+		
+	}
+}
---
+++ src/personnage/Humain.java
@@ -0,0 +1,81 @@
+package personnage;
+
+public class Humain {
+	private String nom;
+	private String boisson;
+	private int quantiteArgent;
+	
+	
+	
+	
+	
+	public Humain(String nom, String boisson, int quantiteArgent) {
+		this.nom = nom;
+		this.boisson = boisson;
+		this.quantiteArgent = quantiteArgent;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public int getQuantiteArgent() {
+		return quantiteArgent;
+	}
+	
+	
+	
+	
+	public void parler(String texte) {
+		System.out.println("("+nom+") - " + texte);
+	}
+	
+	public void direBonjour() {
+		this.parler("Bonjour ! Je m'appelle "+nom+ " et j'aime boire du " + boisson+ ".");
+	}
+	
+	public void boire() {
+		this.parler("Mmmm, un bon verre de "+ boisson + "! GLOUPS !");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if(prix > quantiteArgent) {
+			this.parler("Je n'ai plus que "+ quantiteArgent+" sous en poche. "
+					+ "Je ne peux meme pas m'offrir un "+ bien+" a "+prix +" sous");
+	
+		}else {
+			this.parler("J'ai "+quantiteArgent+" sous en poche. "
+					+ "Je vais pouvoir m'offrir " +bien + " a "+ prix+" sous");
+			this.perdreArgent(prix);
+			
+		}
+		
+	}
+	
+	private void gagnerArgent(int gain) {
+		
+		quantiteArgent += gain;
+		
+	}
+	
+	private void perdreArgent(int perte) {
+		quantiteArgent -= perte;
+	}
+	
+	
+	public static void main(String[] args) {
+		Humain prof = new Humain("Prof", "kombucha", 54);
+		prof.direBonjour();
+		prof.acheter("une boisson", 12);
+		prof.boire();
+		prof.acheter("un jeu", 2);
+		prof.acheter("un kimono", 50);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
